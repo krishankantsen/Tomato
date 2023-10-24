753b8417087c3d8b1cd3168186ffdd8c5e1081eb
Krishan Kant Sen <skrishan111213@gmail.com>
2023-10-24 17:45:17 +0530

    nor

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -113,7 +113,7 @@ export default function Home() {
         </button>
       </div>
       <div className="container col ">
-      {foodCat.length !== 0
+      {foodCat && foodCat.length !== 0
   ? foodCat.map((data) => {
       return (
         <div className="row mb-3" key={data._id}>
@@ -143,6 +143,7 @@ export default function Home() {
       );
     })
   : ""}
+
 
       </div>
 
