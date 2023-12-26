9aeaf39f6f4aebab7704d34ad7185ded697f458c
Krishan Kant Sen <75468514+krishankantsen@users.noreply.github.com>
2023-12-26 14:14:31 +0530

    Update DisplayData.js

--- backend/Routes/DisplayData.js
+++ backend/Routes/DisplayData.js
@@ -25,23 +25,25 @@ const mongoose=require("mongoose")
 //     }
 // })
 router.get('/foodData', async (req, res) => {
-    try {
-      const fetchData = async (collectionName) => {
-        const fetchedData = await mongoose.connection.db.collection().find({}).toArray();
-        return fetchedData;
-      };
-  
-      const [foodItems, foodCats] = await Promise.all([
-        fetchData('food_items'),
-        fetchData('foodCategory'),
-      ]);
-  
-      res.send({ foodItems, foodCats });
-    } catch (error) {
-      console.error('Error fetching and sending data:', error);
-      res.status(500).send('Server error');
-    }
-  });
+  try {
+    const fetchData = async (collectionName) => {
+      // Pass the collectionName to mongoose.connection.db.collection()
+      const fetchedData = await mongoose.connection.db.collection(collectionName).find({}).toArray();
+      return fetchedData;
+    };
+
+    const [foodItems, foodCats] = await Promise.all([
+      fetchData('food_items'),
+      fetchData('foodCategory'),
+    ]);
+
+    res.send({ foodItems, foodCats });
+  } catch (error) {
+    console.error('Error fetching and sending data:', error);
+    res.status(500).send('Server error');
+  }
+});
+
   
 
 
