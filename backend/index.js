329f9805b4ebb9d86876a68377feb9e0d120a500
Krishan Kant Sen <75468514+krishankantsen@users.noreply.github.com>
2023-12-26 14:17:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,30 @@
 const express = require("express");
 const app = express();
-const port = process.env.port || 5000;
-require("./db"); // make sure this path is correct and the file exists
+const port = process.env.PORT || 5000; // Fix: use uppercase PORT
+require("./db");
 const cors = require("cors");
 const path = require("path");
 
-// connecting frontend with backend
 app.use(cors());
-
 app.use(express.json());
 
-app.use("/", require("./Routes/CreateUser")); // make sure these routes are correctly defined
+app.use("/", require("./Routes/CreateUser"));
 app.use("/", require("./Routes/DisplayData"));
 app.use("/", require("./Routes/OrderData"));
 
-app.get("/",(req,res)=>{
-  res.json({message:"Hello I am Backend"})
-})
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../frontend/build/index.html")),
-//     function (err) {
-//       res.status(500).send(err);
-//     };
-// });
- 
+app.get("/", (req, res) => {
+  res.json({ message: "Hello I am Backend" });
+});
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
