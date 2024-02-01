9360274a5d386762e640077bf6c9feed423bc66c
Krishan Kant Sen <75468514+krishankantsen@users.noreply.github.com>
2024-02-01 11:03:02 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,8 +6,8 @@ export default function Footer() {
     <div>
     <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
     <div className="col-md-4 d-flex align-items-center">
-      <Link to="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-       
+      <Link to="https://github.com/krishankantsen" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+       Krishan Kant Sen
       </Link>
       <span className="text-muted">@ 2023 Tomato, Inc</span>
     </div>
